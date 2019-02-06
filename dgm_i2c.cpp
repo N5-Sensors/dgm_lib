919da7a7ab37e2d3947b2ce3962850a9810752d5
vfonseca <vfonsec@n5sensors.com>
2019-02-06 11:30:13 -0500

    initial comit

---
+++ dgm_i2c.cpp
@@ -0,0 +1,193 @@
+#include "dgm_i2c.h"
+
+#define WRITE_WAIT_TIME 60
+DGM::DGM(){
+  _addr = 0x55;
+}
+DGM::DGM(uint8_t addr){
+  _addr = addr;
+}
+
+DGM::DGM(uint8_t addr,uint8_t config){
+  _addr = addr;
+  setConfig(config);
+}
+
+void DGM::init(uint8_t addr) {
+  _addr = addr;
+}
+
+
+void DGM::setPwm(uint8_t value) {
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_PWM_POS);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+void DGM::setI2cAddr(uint8_t value) {
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_I2CADDR_POS);
+  Wire.write(value);
+  Wire.endTransmission();
+  delay(WRITE_WAIT_TIME);
+}
+
+void DGM::setConfig(uint8_t value) {
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_CONFIG_POS);
+  Wire.write(value);
+  Wire.endTransmission();
+  delay(WRITE_WAIT_TIME);
+}
+
+
+
+uint8_t DGM::getConfig() {
+  uint8_t received;
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_CONFIG_POS);
+  Wire.endTransmission();
+  Wire.requestFrom(_addr, (uint8_t)1);
+  unsigned int aux = 0;
+  while ( aux < sizeof(received) && Wire.available() > 0 )
+  {
+    received = Wire.read();
+    //Serial.print(received[aux], HEX);
+    aux++;
+  }
+  return received;
+
+}
+
+void DGM::calibrateBridge1() {
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_CONFIG_POS);
+  Wire.write((byte)0x81);
+  Wire.endTransmission();
+  uint8_t value = getConfig();
+  while( (value & CONFIG_CMD_MASK) ){
+    value = getConfig();
+    delay(10);
+  }
+}
+
+void DGM::calibrateBridge2() {
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_CONFIG_POS);
+  Wire.write((byte)0x82);
+  Wire.endTransmission();
+  uint8_t value = getConfig();
+  while( (value & CONFIG_CMD_MASK) ){
+    value = getConfig();
+    delay(10);
+  }
+}
+
+void DGM::setCh1GainMuxCh(uint8_t value) {
+  if( value >= 0 && value <= 3){
+    uint8_t config = getConfig();
+    value = value << 1;
+    config = config | value;
+    setConfig(config);
+  }
+}
+
+void DGM::setCh2GainMuxCh(uint8_t value) {
+  if( value >= 0 && value <= 3){
+    uint8_t config = getConfig();
+    value = value << 3;
+    config = config | value;
+    setConfig(config);
+  }
+}
+
+void DGM::setAdcBufferSize(uint8_t value) {
+  if( value >= 0 && value <= 3){
+    uint8_t config = getConfig();
+    value = value << 5;
+    config = config | value;
+    setConfig(config);
+  }
+}
+
+float DGM::getTemp() {
+  unsigned char received[4];
+  float value;
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_TEMPERATURE_POS);
+  Wire.endTransmission();
+  Wire.requestFrom(_addr, (uint8_t)4);
+  unsigned int aux = 0;
+  while ( aux < sizeof(received) && Wire.available() > 0 )
+  {
+    received[aux] = Wire.read();
+    //Serial.print(received[aux], HEX);
+    aux++;
+  }
+  memcpy(&value, received, 4);
+
+  return value;
+
+}
+
+float DGM::getHumidity() {
+  unsigned char received[4];
+  float value;
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_HUMIDITY_POS);
+  Wire.endTransmission();
+  Wire.requestFrom(_addr, (uint8_t)4);
+  unsigned int aux = 0;
+  while ( aux < sizeof(received) && Wire.available() > 0 )
+  {
+    received[aux] = Wire.read();
+    //Serial.print(received[aux], HEX);
+    aux++;
+  }
+  memcpy(&value, received, 4);
+
+  return value;
+
+}
+
+
+float DGM::getVoltege1() {
+  unsigned char received[4];
+  float value;
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_B1_POS);
+  Wire.endTransmission();
+  Wire.requestFrom(_addr, (uint8_t)4);
+  unsigned int aux = 0;
+  while ( aux < sizeof(received) && Wire.available() > 0 )
+  {
+    received[aux] = Wire.read();
+    //Serial.print(received[aux], HEX);
+    aux++;
+  }
+  memcpy(&value, received, 4);
+
+  return value;
+
+}
+
+float DGM::getVoltege2() {
+  unsigned char received[4];
+  float value;
+  Wire.beginTransmission(_addr);
+  Wire.write(PACKET_B2_POS);
+  Wire.endTransmission();
+  Wire.requestFrom(_addr, (uint8_t)4);
+  unsigned int aux = 0;
+  while ( aux < sizeof(received) && Wire.available() > 0 )
+  {
+    received[aux] = Wire.read();
+    //Serial.print(received[aux], HEX);
+    aux++;
+  }
+  memcpy(&value, received, 4);
+
+  return value;
+
+}
---
+++ dgm_i2c.h
@@ -0,0 +1,51 @@
+#ifndef DGM_I2C_H
+#define DGM_I2C_H
+#include <Arduino.h>
+#include <Wire.h>
+
+//#define dgm_i2c_address ((uint8_t)0x55)
+#define PACKET_CONFIG_POS           (0u)
+#define PACKET_I2CADDR_POS          (PACKET_CONFIG_POS+1u)
+#define PACKET_PWM_POS              (PACKET_I2CADDR_POS+1u)
+#define PACKET_B1_POS               (PACKET_PWM_POS+1u)
+#define PACKET_B2_POS               (PACKET_B1_POS+4u)
+#define PACKET_TEMPERATURE_POS      (PACKET_B2_POS+4u)
+#define PACKET_HUMIDITY_POS         (PACKET_TEMPERATURE_POS+4u)
+
+#define DGM_ADC_BUFFER_SIZE_1 0x00
+#define DGM_ADC_BUFFER_SIZE_10 0x01
+#define DGM_ADC_BUFFER_SIZE_50 0x02
+#define DGM_ADC_BUFFER_SIZE_100 0x03
+
+
+/*CONFIG MASKS*/
+#define CONFIG_PULLUP_MASK (1u) //if 1 pull up active if 0 open drain strong low
+#define CONFIG_B1GAINMUXCH_MASK ((3u)<<1)
+#define CONFIG_B2GAINMUXCH_MASK ((3u)<<3)
+#define CONFIG_ADCAVRGBUFFSIZE_MASK ((3u)<<5)
+#define CONFIG_CMD_MASK ((1u)<<7)
+
+class DGM
+{
+  public:
+    DGM();
+    DGM(uint8_t addr);
+    DGM(uint8_t addr,uint8_t config);
+    void init(uint8_t addr);
+    float getTemp();
+    float getHumidity();
+    float getVoltege1();
+    float getVoltege2();
+    uint8_t getConfig();
+    void setPwm(uint8_t value);
+    void setI2cAddr(uint8_t value);
+    void setConfig(uint8_t value);
+    void setCh1GainMuxCh(uint8_t value);
+    void setCh2GainMuxCh(uint8_t value);
+    void setAdcBufferSize(uint8_t value);
+    void calibrateBridge1();
+    void calibrateBridge2();
+    uint8_t _addr = 0x55;
+};
+
+#endif
